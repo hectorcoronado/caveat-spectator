a4f651bc1beaa5c73ab0d27837d23595a888bcae
hectorcoronado <hectorcoronado.developer@gmail.com>
2020-06-08 23:04:34 -0700

    add auth middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+// middleware
+const auth = require('../../middleware/auth')
+
+// models
+const User = require('../../models/User')
+
 /**
  * @route GET api/auth
  *
  * @desc test route
  * @access public
  */
-router.get('/', (req, res) => res.send('auth route'))
+router.get('/', auth, async (req, res) => {
+  try {
+    // retrieve user, but don't send back password
+    const user = await User.findById(req.user.id).select('-password')
+
+    res.json(user)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Auth middlewware is not functioning properly')
+  }
+})
 
 module.exports = router
